8f714622459febe5e5190b6206565e6c215400d7
Mohammed Islam <aeinul.islam@gmail.com>
2016-10-03 16:24:59 -0700

    retrieving picture

--- imports/startup/both/useraccounts-configuration.js
+++ imports/startup/both/useraccounts-configuration.js
@@ -1,5 +1,6 @@
 // User account configuration that runs on both server and client
 import { AccountsTemplates } from 'meteor/useraccounts:core';
+import {Accounts} from 'meteor/accounts-base';
 
 /**
  * The useraccounts package must be configured for both client and server to work properly.
@@ -30,3 +31,18 @@ AccountsTemplates.configureRoute('resetPwd', {
   name: 'resetPwd',
   path: '/reset-password',
 });
+
+
+Accounts.onCreateUser(function(options, user) {
+    if (options.profile) {
+        options.profile.picture = "http://graph.facebook.com/" + user.services.facebook.id + "/picture/?type=large";
+        user.profile = options.profile;
+    }
+    return user;
+});
+
+
+
+
+
+
