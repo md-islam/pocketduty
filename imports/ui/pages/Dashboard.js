e859a153bb8fe347f589af7183b2de0691866e21
Mitchell Waldman <mitchswaldman@gmail.com>
2016-10-09 16:14:35 -0700

    Subscribe to shopping duties on dashboard page.

--- imports/ui/pages/Dashboard.js
+++ imports/ui/pages/Dashboard.js
@@ -1,7 +1,18 @@
 import {Template} from 'meteor/templating';
+import { ShoppingDuties} from '../../api/shoppingDuties/shoppingDuties.js';
 import './Dashboard.html';
+import '../components/shoppingDuty.js';
 
 Template.Dashboard.onCreated(function(){
 	this.autorun(() => {
+		this.subscribe('shoppingDuties', {});
 	});
 });
+
+Template.Dashboard.helpers({
+	shoppingDuties() {
+		console.log("Getting shopping duties");
+		duties = ShoppingDuties.find();
+		return duties;
+	}
+})
