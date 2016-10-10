77c732e66d02dd9ac3cb7fb70af7950377e98783
Jakub Kalinowski <j.kalinowski.86@gmail.com>
2016-10-09 17:49:58 -0700

    Added DutiesScreen in NewDuty
    routing issue exists

--- imports/ui/pages/NewDuty.js
+++ imports/ui/pages/NewDuty.js
@@ -1,10 +1,4 @@
 import {Template } from 'meteor/templating';
-import { ShoppingDuties } from '../../api/shoppingDuties/shoppingDuties.js';
 import './NewDuty.html';
-import '../components/insertShoppingDutiesForm.js';
+import '../partials/DutiesScreen.html';
 
-Template.NewDuty.onCreated(function NewDutyCreated(){
-	this.autorun(() => {
-	    this.subscribe('shoppingDuties', { });
-	  });
-})
