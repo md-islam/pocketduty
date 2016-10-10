2459c20616c2b0a24351e8107c1208b648274c35
Jakub Kalinowski <j.kalinowski.86@gmail.com>
2016-10-09 17:49:58 -0700

    Added DutiesScreen in NewDuty
    routing issue exists

--- imports/ui/pages/NewDuty.js
+++ imports/ui/pages/NewDuty.js
@@ -1,3 +1,4 @@
 import { ShoppingDuties } from '../../api/shoppingDuties/shoppingDuties.js';
 import './NewDuty.html';
+import '../partials/DutiesScreen.html';
 import '../components/insertShoppingDutiesForm.js';
