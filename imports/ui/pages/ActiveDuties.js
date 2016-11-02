e0f14b5a5274e3e825cad8559ed3570ef7b94167
enkhturbadamsaikhan <enkhtursjsu@gmail.com>
2016-11-02 11:48:20 -0700

    Employer can see in progress, completed duties

--- imports/ui/pages/ActiveDuties.js
+++ imports/ui/pages/ActiveDuties.js
@@ -2,36 +2,57 @@ import {Template} from 'meteor/templating';
 import { ShoppingDuties} from '../../api/shoppingDuties/shoppingDuties.js';
 import { AcademicDuties} from '../../api/academicDuties/academicDuties.js';
 import { LaundryDuties} from '../../api/laundryDuties/laundryDuties.js';
+import { TransportDuties} from '../../api/transportDuties/transportDuties.js';
 import './ActiveDuties.html';
 import '../components/shoppingDuty.html';
 import '../components/academicDuty.js';
 import '../components/laundryDuty.html';
+import '../components/transportDuty.html';
+
+import { AcceptableDutyStatuses } from '../../api/duties/duties.js';
 
 Template.ActiveDuties.onCreated(function(){
 	this.autorun(() => {
-		this.subscribe('shoppingDuties', {});
-		this.subscribe('academicDuties', {});
-		this.subscribe('laundryDuties', {})
+		this.subscribe('laborShoppingIncomplete', {});
+		// this.subscribe('academicDuties', {});
+		// this.subscribe('laundryDuties', {});
+		// this.subscribe('transportDuties', {});
 	});
 });
 
 Template.ActiveDuties.helpers({
 	shoppingDuties() {
-		//var status = ShoppingDuties.find({status: Meteor.status()});
+		// var status = ShoppingDuties.find({status: ShoppingDuties.find(status)});
+		// console.log(status);
 		//if (status === "New") {
-			console.log("Getting NEW shopping duties");
-			duties = ShoppingDuties.find({userId: Meteor.userId()});
+			console.log("Getting Assigned shopping duties");
+			// var myCursor = ShoppingDuties.find({userId: Meteor.userId()});
+			// var myDocument = myCursor.hasNext() ? myCursor.next() : null;
+			// if (myDocument) {
+   //  			var myStatus = myDocument.status;
+   //  			console.log (tojson(myName));
+			// }
+			
+			// window.ShoppingDuties = ShoppingDuties;
+			var obj = ShoppingDuties.find({userId: Meteor.userId()}, {"status": "Assigned"}).fetch();
+			console.log(obj);
+			duties = ShoppingDuties.find({status: "Assigned"});
 			return duties;
 		// }
-	},
-	academicDuties() {
-		console.log("Getting NEW academic duties");
-		return AcademicDuties.find({userId: Meteor.userId()});
-	},
-	laundryDuties() {
-		console.log("Getting NEW laundry duties");
-		return LaundryDuties.find({userId: Meteor.userId()});
 	}
+	// ,
+	// academicDuties() {
+	// 	console.log("Getting NEW academic duties");
+	// 	return AcademicDuties.find({userId: Meteor.userId()});
+	// },
+	// laundryDuties() {
+	// 	console.log("Getting NEW laundry duties");
+	// 	return LaundryDuties.find({userId: Meteor.userId()});
+	// },
+	// transportDuties() {
+	// 	console.log("Getting NEW transport duties");
+	// 	return TransportDuties.find({userId: Meteor.userId()});
+	// }
 	// text: {
 	// 	var status = ShoppingDuties.find({status: Meteor.status()});
 	// 	console.log(status);
--- imports/ui/pages/PastDuties.js
+++ imports/ui/pages/PastDuties.js
@@ -7,11 +7,13 @@ import '../components/shoppingDuty.html';
 import '../components/academicDuty.js';
 import '../components/laundryDuty.html';
 
+import { AcceptableDutyStatuses } from '../../api/duties/duties.js';
+
 Template.PastDuties.onCreated(function(){
 	this.autorun(() => {
-		this.subscribe('shoppingDuties', {});
-		this.subscribe('academicDuties', {});
-		this.subscribe('laundryDuties', {})
+		this.subscribe('laborShoppingCompleted', {});
+		// this.subscribe('academicDuties', {});
+		// this.subscribe('laundryDuties', {})
 	});
 });
 
@@ -19,17 +21,19 @@ Template.PastDuties.helpers({
 	shoppingDuties() {
 		//var status = ShoppingDuties.find({status: Meteor.status()});
 		//if (status === "New") {
-			console.log("Getting NEW shopping duties");
-			duties = ShoppingDuties.find({userId: Meteor.userId()});
+			console.log("Getting Completed shopping duties");
+			// duties = ShoppingDuties.find({userId: Meteor.userId()});
+			duties = ShoppingDuties.find({status: "Complete"});
 			return duties;
 		// }
-	},
-	academicDuties() {
-		console.log("Getting NEW academic duties");
-		return AcademicDuties.find({userId: Meteor.userId()});
-	},
-	laundryDuties() {
-		console.log("Getting NEW laundry duties");
-		return LaundryDuties.find({userId: Meteor.userId()});
 	}
+	// ,
+	// academicDuties() {
+	// 	console.log("Getting Completed academic duties");
+	// 	return AcademicDuties.find({userId: Meteor.userId()});
+	// },
+	// laundryDuties() {
+	// 	console.log("Getting Completed laundry duties");
+	// 	return LaundryDuties.find({userId: Meteor.userId()});
+	// }
 });
