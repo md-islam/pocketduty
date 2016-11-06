9987b0a2be519a2164f005647df6807cc23d673f
kkhattab <kareemkhattab925@gmail.com>
2016-11-06 11:21:13 -0800

    never mind forms not working but are visible

--- imports/ui/components/transportDuty.js
+++ imports/ui/components/transportDuty.js
@@ -2,6 +2,6 @@ import { Template } from 'meteor/templating';
 import { TransportDuties } from '../../api/transportDuties/transportDuties.js';
 import './transportDuty.html';
 
-Template.ShoppingDuty.onCreated(function(){
+Template.TransportDuty.onCreated(function(){
 	console.log("Created transportDuty duty template with date context ", this.data);
 }); 
